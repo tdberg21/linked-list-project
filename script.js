580775329ae7f94791549398f9b4ed58fa380dd4
Tory Dannenberg <rfdberg@gmail.com>
2018-03-24 14:16:37 -0600

    Add function enable buttons

--- script.js
+++ script.js
@@ -5,24 +5,31 @@ var websiteUrl = document.querySelector('.website-url');
 var enterButton = document.querySelector('.yellow-button');
 var readButton = document.querySelector('.read-button');
 var deleteButton = document.querySelector('.delete-button');
+var cardContainer = document.querySelector('.cardContainer');
 
+console.log(typeof websiteTitle.value);
 
-enterButton.addEventListener('click', checkFields);
+enterButton.addEventListener('click', appendCard);
 readButton.addEventListener('click', markAsRead);
 deleteButton.addEventListener('click', deleteLink);
-
-function checkFields() {
-  console.log('click');
-  event.preventDefault(checkFields);
-}
+websiteTitle.addEventListener('keyup', enableButton);
+websiteUrl.addEventListener('keyup', enableButton);
 
 function markAsRead() {
   console.log('click');
   event.preventDefault(markAsRead);
-}
+};
 
 function deleteLink() {
   console.log('click');
   event.preventDefault(deleteLink);
-}
+};
+
+function enableButton() {
+  if (websiteTitle.value === '' || websiteUrl.value === '') {
+    enterButton.disabled = true;
+  } else {
+    enterButton.disabled = false;
+  };
+};
 
