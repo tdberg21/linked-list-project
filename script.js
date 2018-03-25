7a296d35312657c6bed1328e140a6e67af14df87
Tory Dannenberg <rfdberg@gmail.com>
2018-03-25 14:35:24 -0600

    Add alert for empty title and url fields work on getting read button to toggle class unsuccesfully

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-
 var $websiteTitle = $('.website-title');
 var $websiteUrl = $('.website-url');
 var $enterButton = $('.yellow-button');
 var $readButton = $('.read-button');
 var $deleteButton = $('.delete-button');
 var $cardContainer = $('.cardContainer');
-
-enableButton();
-
+var $handleClick = function() {
+  console.log( 'something was clicked' );
+};
 
 $enterButton.on('click', getInfo);
-$readButton.on('click', markAsRead);
+$readButton.on('click', read);
 $deleteButton.on('click', deleteLink);
-$websiteTitle.on('keyup', enableButton);
-$websiteUrl.on('keyup', enableButton);
-
+// $websiteTitle.on('keyup', enableButton);
+// $websiteUrl.on('keyup', enableButton);
 
 function getInfo(event) {
   event.preventDefault();
-  isValidUrl($websiteUrl.val())
-
-}
+  isFormComplete();
+};
 
 function prependCard(event) {
   $('ul').prepend(`
     <li class="cards">
       <h2>${$websiteTitle.val()}</h2>
       <hr>
-      <a href="">${$websiteUrl.val()}</a>
+      <a href="${$websiteUrl.val()}">${$websiteUrl.val()}</a>
       <hr>
       <button class="readDeleteButtons read-button">Read</button>
       <button class="readDeleteButtons delete-button">Delete</button>
     </li>`);
 };
 
-function markAsRead(event) {
+function read(event) {
   console.log('click');
+  $('li').toggleClass('read');
   event.preventDefault();
 };
 
@@ -44,20 +42,20 @@ function deleteLink(event) {
   event.preventDefault();
 };
 
-
-function enableButton() {
+function isFormComplete() {
   if ($websiteTitle.val() && $websiteUrl.val()) {
     $enterButton.prop('disabled', false);
+    isValidUrl($websiteUrl.val());
   } else {
-    $enterButton.prop('disabled', true);
-  }
-}
+    alert('Please enter title and URL.');
+  };
+};
 
 function isValidUrl(url) {
   if (/(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(url) === false) {
-  alert('please enter valid url')
+    alert('please enter valid url');
   } else {
     prependCard();
-  }
+  };
 };
 
